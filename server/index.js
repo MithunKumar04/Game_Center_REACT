367e2e629f615559b0d7c1ad63285602eb4715c5
MithunKumar04 <mithunkumarb.22aim@kongu.edu>
2025-02-15 15:24:37 +0530

    connecting vercel

--- server/index.js
+++ server/index.js
@@ -13,11 +13,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "https://mithun-aksh-nithika.netlify.app/", // Allow only this frontend
-  methods: "GET, POST, PUT, DELETE,OPTIONS",
-  allowedHeaders: "Content-Type, Authorization"
-}));
+app.use(cors());
 const url = process.env.MONGO_URL;
 mongoose.connect(url);
 
