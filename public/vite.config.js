ab4df2a736ab5eedda74dd292f8eb2dd36c892b7
MithunKumar04 <mithunkumarb.22aim@kongu.edu>
2025-02-15 12:38:14 +0530

    connecting to render

--- public/vite.config.js
+++ public/vite.config.js
@@ -4,7 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  build: {
-    outDir: "dist"  // Ensure the build output goes into "dist"
-  }
+  
 })
