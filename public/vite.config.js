0cfe5f9b77d77723551abcdd738c8a77b0d28487
MithunKumar04 <mithunkumarb.22aim@kongu.edu>
2025-02-15 16:07:01 +0530

    final2

--- public/vite.config.js
+++ public/vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
-    },
-  },
   base: "./",  // Ensure relative path for Netlify
   build: {
     outDir: "dist",  // Ensure output goes to the correct folder
@@ -17,4 +10,4 @@ export default defineConfig({
   optimizeDeps: {
     include: ["slick-carousel"],
   },
-})
+});
